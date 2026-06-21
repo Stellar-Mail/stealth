c6ba2296c154fc299efb8ea02b06dcd88915979e
xx7412421-cloud <xx7412421@gmail.com>
2026-06-21 10:33:31 +0800

    Export deadline input guards

--- tools/v2/individual/deadline-detector/services/index.ts
+++ tools/v2/individual/deadline-detector/services/index.ts
@@ -1,1 +1,7 @@
 export { detectDeadlines, sortDetectedDeadlines } from "./deadline-detector.service";
+export {
+  DEADLINE_INPUT_LIMITS,
+  cleanDeadlineText,
+  normalizeDeadlineMessages,
+} from "./input-guards";
+export type { DeadlineInputGuardOptions, DeadlineInputLimits } from "./input-guards";
