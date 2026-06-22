4d1ca32baf4502849765616770f0de070cdae3e6
Developer <dev@stealth.xyz>
2026-06-22 02:40:35 +0100

    feat: add account switcher with multi-profile support

---
+++ src/features/account-switcher/index.ts
@@ -0,0 +1,12 @@
+export { AccountSwitcher } from "./AccountSwitcher";
+export { AccountSwitcherProvider, useAccountSwitcher } from "./AccountSwitcherContext";
+export { useAccounts } from "./useAccounts";
+export { useDraftState } from "./useDraftState";
+export type {
+  AccountProfile,
+  AccountSwitcherDraftState,
+  NetworkType,
+  SyncState,
+} from "./types";
+export type { UseAccountsReturn } from "./useAccounts";
+export type { UseDraftStateReturn } from "./useDraftState";
